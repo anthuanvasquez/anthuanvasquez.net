d4c9e29e23c41d030c9b57c221cbfc2e0fc84779
Anthuan Vasquez <anthuan@minnekdigital.com>
2024-04-23 08:33:09 -0400

    chore: remove old components and unnecessary files

--- plugins/contentful.js
+++
@@ -1,9 +0,0 @@
-const contentful = require('contentful')
-
-const client = contentful.createClient({
-  space: process.env.CTF_SPACE_ID,
-  accessToken: process.env.CTF_CDA_ACCESS_TOKEN,
-  resolveLinks: true
-})
-
-module.exports = client
