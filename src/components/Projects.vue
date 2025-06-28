35967e4387067426beb59cfbf72473cf672b61f7
Anthuan Vasquez <me@anthuanvasquez.net>
2025-06-28 08:40:26 -0400

    refactor: project component

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,33 +1,24 @@
 <script setup lang="ts">
-export declare type IProjects = {
-  name: string;
-  description: string;
-};
+import type { Project } from '~/types';
 
-const { supabase } = useSupabase();
-const projects = ref([] as IProjects[]);
-
-try {
-  const { data, error } = await supabase.from('projects').select();
-
-  projects.value = data as IProjects[];
-} catch (error) {
-  error.value = true;
-  projects.value = [];
-}
+const {
+  data: projects,
+  error,
+  pending,
+} = await useGetFetch<Project[]>('/api/projects');
 </script>
 
 <template>
   <div class="container mx-auto">
-    <div v-if="!error" class="grid grid-cols-12 gap-8">
+    <div v-if="projects" class="grid grid-cols-12 gap-8">
       <div v-for="(project, index) in projects" :key="index" class="col-span-6">
         <div
-          class="border border-slate-800 p-4 h-full rounded-lg relative text-center"
+          class="relative h-full rounded-lg border border-slate-800 p-4 text-center"
         >
-          <h2 class="text-2xl font-bold">
+          <h2 class="font-firacode text-2xl font-medium">
             {{ project.name }}
           </h2>
-          <p class="text-md my-3">
+          <p class="text-md my-3 font-light">
             {{ project.description }}
           </p>
         </div>
