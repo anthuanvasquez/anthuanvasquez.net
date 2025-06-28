ff9d03d65e2a1e6fa791d2aae70235b9e96c69fd
Anthuan Vasquez <me@anthuanvasquez.net>
2025-06-28 08:42:56 -0400

    feat: move all types to /types

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,32 @@ export type HeroIconName = keyof typeof solid | keyof typeof outline;
 export type CustomIconName = keyof typeof custom;
 export type IconType = 'solid' | 'outline' | 'custom';
 export type IconName = HeroIconName | CustomIconName;
+
+export type Project = {
+  name: string;
+  description: string;
+};
+
+export type Experience = {
+  name: string;
+  description: string;
+  position: string;
+  date: string;
+  coding: string;
+  country: IconName;
+  projects?: {
+    name: string;
+    description: string;
+    skills: string;
+  }[];
+};
+
+export type Skill = {
+  name: string;
+  category: string;
+};
+
+export type Service = {
+  name: string;
+  icon: IconName;
+};
