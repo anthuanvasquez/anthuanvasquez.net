970327a369b84a806fc0aa973a40fac1071ec67d
Anthuan Vasquez <me@anthuanvasquez.net>
2025-06-28 08:43:34 -0400

    refactor: footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,52 +1,10 @@
 <template>
-  <footer id="footer" class="footer py-8">
-    <div class="container mx-auto">
-      <div class="grid grid-cols-12 gap-8">
-        <div class="col-span-4">
-          <div class="space-y-4">
-            <h3 class="text-2xl font-bold">Thank you for visiting my page.</h3>
-            <p>
-              When I'm not writing code you can find me trying to learn new
-              things, playing video games, trying new restaurants, trying out
-              new tech, taking a coke or making music.
-            </p>
-          </div>
-        </div>
-        <div class="col-span-4">
-          <div class="space-y-4">
-            <h3 class="text-2xl font-bold">
-              People are the essence of the tech industry.
-            </h3>
-            <p>
-              Without people, we would have no clear purpose in our work. I
-              believe in helping people feel welcomed and included when it comes
-              to my work and who I am as a person.
-            </p>
-          </div>
-        </div>
-        <div class="col-span-4">
-          <div class="space-y-4">
-            <h3 class="text-2xl font-bold">Get in touch.</h3>
-            <p>
-              If you have any questions feel free to reach out at
-              <a href="mailto:me@anthuanvasquez.net"
-                ><i class="fas fa-envelope" /> me@anthuanvasquez.net</a
-              >. I'm excited for our session and to meet you!.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div id="copyrights" class="mt-8">
-      <div class="container mx-auto text-center">
-        <p class="text-sm">
-          Copyrights 2019. <span>Designed</span>, <span>Created</span> and
-          <span>Develop</span> by
-          <strong><a href="https://und3fined.dev">und3fined.dev</a></strong
-          >.
-        </p>
-      </div>
-    </div>
+  <footer id="footer" class="py-8 text-center">
+    <p class="text-sm">
+      Copyrights 2025. <span>Designed</span>, <span>Created</span> and
+      <span>Develop</span> by
+      <strong><a href="https://anthuanvasquez.net">Anthuan</a></strong
+      >.
+    </p>
   </footer>
 </template>
