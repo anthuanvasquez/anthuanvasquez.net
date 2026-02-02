4d039e95c4d7ab5a9a60022281534bc8eba925aa
Anthuan Vasquez <me@anthuanvasquez.net>
2026-02-02 04:44:40 -0400

    chore(lint): update prettier and eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,23 @@
 import globals from 'globals';
 import eslint from 'eslint';
-import vueParser from 'vue-eslint-parser'
-import tsparser from '@typescript-eslint/parser'
+import vueParser from 'vue-eslint-parser';
+import tsparser from '@typescript-eslint/parser';
 import prettier from 'eslint-plugin-prettier';
-import vue from 'eslint-plugin-vue'
-import tseslint from '@typescript-eslint/eslint-plugin'
+import vue from 'eslint-plugin-vue';
+import tseslint from '@typescript-eslint/eslint-plugin';
 import nuxt from 'eslint-plugin-nuxt';
 import vitest from 'eslint-plugin-vitest';
 
 export default [
   {
     ignores: [
-      "**/*.spec.js",
-      ".nuxt",
-      ".cache",
-      ".nitro",
-      "node_modules",
-      "dist",
-
-    ]
+      '**/*.spec.js',
+      '.nuxt',
+      '.cache',
+      '.nitro',
+      'node_modules',
+      'dist',
+    ],
   },
   {
     files: ['**/*.ts', '**/*.vue'],
@@ -28,17 +27,17 @@ export default [
         parser: tsparser,
         ecmaVersion: 2025,
         sourceType: 'module',
-        extraFileExtensions: ['.vue']
+        extraFileExtensions: ['.vue'],
       },
       globals: {
         ...globals.browser,
         ...globals.es2025,
-      }
+      },
     },
     settings: {
-      "better-tailwindcss": {
-        entryPoint: "src/assets/main.css",
-      }
+      'better-tailwindcss': {
+        entryPoint: 'src/assets/main.css',
+      },
     },
     plugins: {
       '@typescript-eslint': tseslint,
@@ -57,7 +56,7 @@ export default [
       'vue/require-default-prop': 'off',
       'vue/no-multiple-template-root': 'off',
       'vue/multi-word-component-names': 'off',
-      'vue/html-self-closing': 'off'
-    }
+      'vue/html-self-closing': 'off',
+    },
   },
-]
+];
