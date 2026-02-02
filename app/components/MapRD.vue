220df7d3abf78bd8635d51cb8aef6e441bcbf48e
Anthuan Vasquez <me@anthuanvasquez.net>
2026-02-02 04:41:37 -0400

    feat: mapbox improvements

---
+++ app/components/MapRD.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="relative h-[500px] w-full">
+    <MapboxMap
+      map-id="map-rd"
+      class="h-full w-full"
+      :options="{
+        style: 'mapbox://styles/mapbox/dark-v11',
+        center: [-70.692, 19.442],
+        zoom: 4,
+        projection: 'equalEarth',
+        scrollZoom: false,
+        maxBounds: [
+          [-118, -56],
+          [-34, 33],
+        ],
+      }"
+    >
+      <MapboxDefaultMarker
+        marker-id="marker-rd"
+        :lnglat="[-70.692, 19.442]"
+        color="#27bcfd"
+      />
+    </MapboxMap>
+  </div>
+</template>
+
+<script setup lang="ts">
+// nuxt-mapbox handles the loading and lifecycle automatically
+</script>
